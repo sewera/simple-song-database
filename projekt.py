1256d3da27f2de80572fda37a0535181d93bc705
Blazej Sewera <code@sewera.dev>
2018-12-29 23:57:36 +0100

    Frontend and language switching update

--- projekt.py
+++ projekt.py
@@ -1,6 +1,6 @@
 """
 'Song Database' Simple Python Program
-Version: v0.1.0.1 Beta Backend, Alpha Frontend
+Version: v0.2 Beta
 Author: Blazej Sewera
 Copyright 2018
 E-mail: blazejok1@wp.pl
@@ -8,6 +8,7 @@ Webpage: [https://github.com/jazzsewera]
 Python version: 3.7.1
 Dependencies: dill (!), collections, operator, re, json
 (!) - non-standard dependency
+Additional files: lang/pl.json and lang/en.json, also copyrighted 2018
 Documentation in Polish due to the program being a university project.
 """
 from collections import namedtuple as nt
@@ -41,9 +42,9 @@ class SongDatabase(object):
     bin_filename = 'songs.pickle' # przechowuje nazwe pliku binarnego
     searchresults = [] # przechowuje liste utworzona w wyniku wyszukiwania `search_in_songlist()`
 
-    def add_song(self, new_artist=None, new_album=None, new_title=None, new_year=None, new_duration_m=None, new_duration_s=None):
+    def add_song(self, new_artist, new_album, new_title, new_year, new_duration_m, new_duration_s):
         """
-        Metoda dodajaca utwor do `songlist`. Wykonana bez ktoregokolwiek z argumentow, dopyta uzytkownika o nie.
+        Metoda dodajaca utwor do `songlist`.
         Args:
             new_artist - nazwa artysty
             new_album - nazwa albumu, itd.
@@ -51,19 +52,6 @@ class SongDatabase(object):
             db.add_song('Toto', 'Toto IV', 'Africa', 1984, 4, 35)
         """
         song = nt('song', 'artist album title year duration_m duration_s') # namedtuple (krotka, odpowiednik struktury) przechowujaca wszystkie informacje o utworze
-
-        if new_artist == None:
-            new_artist = input('Artist: ')
-        if new_album == None:
-            new_album = input('Album: ')
-        if new_title == None:
-            new_title = input('Title: ')
-        if new_year == None:
-            new_year = int( input('Year: ') )
-        if new_duration_m == None:
-            new_duration_m = int( input('Duration (minutes): ') )
-        if new_duration_s == None:
-            new_duration_s = int( input('Duration (seconds): ') )
         self.songlist.append( song(artist=new_artist, album=new_album, title=new_title, year=new_year, duration_m=new_duration_m, duration_s=new_duration_s) )
 
     def write_to_text_file(self, filename=filename):
@@ -138,7 +126,13 @@ class SongDatabase(object):
         Example:
             db.search_in_songlist(input('Wyszukaj po tytule: '), search_by='title')
         """
-        self.searchresults = [song for song in self.songlist if re.search(search_string, attrgetter(search_by)(song), re.IGNORECASE)]
+        if search_by == 'year':
+            self.searchresults = [song for song in self.songlist if int(search_string) == song.year]
+        else:
+            self.searchresults = [song for song in self.songlist if re.search(search_string, attrgetter(search_by)(song), re.IGNORECASE)]
+
+    def remove_songs(self):
+        self.songlist = [song for song in self.songlist if song not in self.searchresults]
 
 class SongDatabaseHead(object):
     """
@@ -158,27 +152,73 @@ class SongDatabaseHead(object):
 
     def start(self):
         _ = self.messages.get
-        menu = {'h': self.help, 's': self.search, 'p': self.pretty_print_songlist, 'e': self.exit}
+
+        menu = {
+            'h': self.help,
+            'a': self.add_song,
+            's': self.search,
+            'p': self.pretty_print_songlist,
+            'r': self.remove_songs,
+            'fo': self.db.open_from_text_file,
+            'fob': self.db.open_from_binary_file,
+            'fw': self.db.write_to_text_file,
+            'fwb': self.db.write_to_binary_file,
+            'fc': self.change_text_filename,
+            'fcb': self.change_binary_filename,
+            'cl': self.change_language,
+            'e': exit
+        }
+
         print(_('greeting'))
-        choice = input(_('choice_prompt'))
-        menu.get(choice, self.invalid_input)()
+        choice = ''
+        while not choice == 'e' and not choice == 'cl':
+            choice = input(_('choice_prompt')).lower()
+            menu.get(choice, self.invalid_input)()
 
     def help(self):
         _ = self.messages.get
-        print('help')
+        print(_('help'))
 
     def invalid_input(self):
         _ = self.messages.get
         print(_('invalid_input'))
 
+    def add_song(self):
+        """
+        Metoda dodajaca utwor do `songlist` w obiekcie db interaktywnie.
+        """
+        _ = self.messages.get
+        new_artist = input(_('artist') + ': ')
+        new_album = input(_('album') + ': ')
+        new_title = input(_('title') + ': ')
+        new_year = int( input(_('year') + ': ') )
+        new_duration_m = int( input(_('duration_m') + ': ') )
+        new_duration_s = int( input(_('duration_s') + ': ') )
+
+        self.db.add_song(new_artist, new_album, new_title, new_year, new_duration_m, new_duration_s)
+
     def search(self):
+        """
+        Metoda wyszukujaca i (w przypadku argumentu False) drukujaca na ekran liste utworow spelniajacych kryteria.
+        """
         _ = self.messages.get
         print(_('search_main'))
-        criteria = input(_('search_choice'))
+        criteria = 'h'
+        while criteria == 'h':
+            print(_('criteria_help'))
+            criteria = input(_('search_choice')) # like 'al' when empty
         search_string = input(_('search_string_prompt'))
-        if criteria == '': criteria = 'album'
-        self.db.search_in_songlist(search_string, search_by=criteria)
+        crit_dict = {'al': 'album', 'ar': 'artist', 't': 'title', 'y': 'year'}
+        self.db.search_in_songlist(search_string, search_by=crit_dict.get(criteria, 'album'))
         self.pretty_print_songlist(searchresults=True)
+
+    def remove_songs(self):
+        """
+        Metoda wyszukujaca i usuwajaca utwory spelniajace kryteria.
+        """
+        _ = self.messages.get
+        self.search()
+        self.db.remove_songs() if input(_('ensure')).lower() == 'y' else print(_('aborted'))
 
     def pretty_print_songlist(self, searchresults=False):
         """
@@ -203,10 +243,44 @@ class SongDatabaseHead(object):
                     print('{0:<25s}|{1:<25s}|{2:<30s}|{3:>6}|{4:>3}:{5:>2}'.format(song.artist, song.album, song.title, song.year, song.duration_m, song.duration_s))
         print() # empty line
 
-    def exit(self):
-        pass
+    def change_text_filename(self):
+        """
+        Metoda pozwalajaca zmienic domyslna nazwe pliku tekstowego.
+        """
+        _ = self.messages.get
+        print(_('change_text_filename'), end='')
+        new_filename = input()
+        if not new_filename == '':
+            self.db.filename = new_filename
+        else:
+            self.db.filename = 'songs.json'
 
-dbh = SongDatabaseHead('pl')
+    def change_binary_filename(self):
+        """
+        Metoda pozwalajaca zmienic domyslna nazwe pliku binarnego.
+        """
+        _ = self.messages.get
+        print(_('change_binary_filename'), end='')
+        new_bin_filename = input()
+        if not new_bin_filename == '':
+            self.db.bin_filename = new_bin_filename
+        else:
+            self.db.bin_filename = 'songs.pickle'
+
+    def change_language(self):
+        """
+        Metoda zmieniajaca jezyk (odczytujaca inny plik z komunikatami)
+        """
+        if self.lang == 'pl': self.lang = 'en'
+        else: self.lang = 'pl'
+        self.messages = {}
+        with open(self.filenames.get(self.lang), 'r') as langfile:
+            self.messages = json.load(langfile)
+        self.start()
+
+
+dbh = SongDatabaseHead('pl') # change to en if you want the app to start in English
+
 dbh.start()
 """
 Poki co, napisana jest wiekszosc backendu i tylko mala czesc frontendu.
@@ -219,5 +293,10 @@ Changelog:
         dodana klasa frontendu (SongDatabaseHead)
         przeniesienie metody pretty_print_songlist do klasy frontendu
         dodana obsluga plikow jezykowych w formacie json
+    v0.2 Beta:
+        dopracowany frontend
+        dodana metoda pozwalajaca usuwac elementy z listy
+        dodane metody do frontendu (wszystkie wywolujace metody w klasie SongDatabase)
+        dodana metoda zmieniajaca jezyk
 Dokumentacja stworzona na podstawie zalecen Google dot. dokumentacji w Pythonie.
 """
